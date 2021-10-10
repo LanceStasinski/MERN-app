992d7750c0118492c35b82d2d0e7f559d1e7e048
LanceStasinski <lance.stasinski@gmail.com>
2021-10-10 13:57:31 -0400

    feat: Use two-way binding and state to update goals list

--- react-refresher/src/App.tsx
+++ react-refresher/src/App.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import GoalList from "./components/GoalList/GoalList";
-import Goal from './models/goal'
+import Goal from "./models/goal";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 function App() {
-  const courseGoals = [
-    new Goal('Goal 1'),
-    new Goal('Goal 2'),
-    new Goal('Goal 3')
-  ];
+  const [courseGoals, setCourseGoals] = useState<Goal[]>([
+    new Goal("Goal 1"),
+    new Goal("Goal 2"),
+    new Goal("Goal 3"),
+  ]);
+
+  const addNewGoalHandler = (newGoal: Goal) => {
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  };
 
   return (
     <div>
       <h2 className="goal-list">Course Goals</h2>
-      <NewGoal />
-      <GoalList goals={courseGoals}/>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
--- react-refresher/src/components/NewGoal/NewGoal.tsx
+++ react-refresher/src/components/NewGoal/NewGoal.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./NewGoal.module.css";
+import Goal from "../../models/goal";
 
-const NewGoal: React.FC = () => {
+const NewGoal: React.FC<{ onAddGoal: (newGoal: Goal) => void }> = (props) => {
+  const [enteredText, setEnteredText] = useState('')
+
+
+  const addGoalHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newGoal = new Goal(enteredText);
+
+    props.onAddGoal(newGoal);
+
+    setEnteredText('')
+  };
+
+  const textChangeHandler = (event: React.FormEvent<HTMLInputElement>): void => {
+    setEnteredText(event.currentTarget.value);
+  };
+
   return (
-    <form className={classes['new-goal']}>
-      <input type="text" />
+    <form className={classes["new-goal"]} onSubmit={addGoalHandler}>
+      <input type="text" onChange={textChangeHandler} value={enteredText} />
       <button type="submit">Add Goal </button>
     </form>
   );
