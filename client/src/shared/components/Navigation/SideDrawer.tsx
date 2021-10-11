396308be9480e7b935e767b1fa672fb34703311a
LanceStasinski <lance.stasinski@gmail.com>
2021-10-11 14:01:48 -0400

    feat: Use portals for rendering the SideDrawer

--- client/src/shared/components/Navigation/SideDrawer.tsx
+++ client/src/shared/components/Navigation/SideDrawer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import ReactDOM  from "react-dom";
 
 import classes from './SideDrawer.module.css';
 
 const SideDrawer: React.FC = (props) => {
+  const content = <aside className={classes['side-drawer']}>{props.children}</aside>;
+  const drawerHook = document.getElementById('drawer-hook') as HTMLElement;
   return (
-    <aside className={classes['side-drawer']}>{props.children}</aside>
-  )
+    ReactDOM.createPortal(content, drawerHook)
+  );
 }
 
 export default SideDrawer;
