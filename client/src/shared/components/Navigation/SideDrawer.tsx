16da8c16d3d0af35883a0667e10d0b8b368b4daf
LanceStasinski <lance.stasinski@gmail.com>
2021-10-11 14:52:00 -0400

    feat: Animate SideDrawer

--- client/src/shared/components/Navigation/SideDrawer.tsx
+++ client/src/shared/components/Navigation/SideDrawer.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
-import classes from './SideDrawer.module.css';
+import classes from "./SideDrawer.module.css";
 
-const SideDrawer: React.FC = (props) => {
-  const content = <aside className={classes['side-drawer']}>{props.children}</aside>;
-  const drawerHook = document.getElementById('drawer-hook') as HTMLElement;
-  return (
-    ReactDOM.createPortal(content, drawerHook)
+const SideDrawer: React.FC<{show: boolean, onClick: () => void}> = (props) => {
+  const nodeRef = React.useRef(null)
+  const content = (
+    <CSSTransition
+
+      in={props.show}
+      timeout={200}
+      classNames={"slide-in-left"}
+      mountOnEnter
+      unmountOnExit
+      nodeRef={nodeRef} //remove strict mode warning
+    >
+      <aside ref={nodeRef} className={classes["side-drawer"]} onClick={props.onClick}>{props.children}</aside>
+    </CSSTransition>
   );
-}
+  const drawerHook = document.getElementById("drawer-hook") as HTMLElement;
+  return ReactDOM.createPortal(content, drawerHook);
+};
 
 export default SideDrawer;
