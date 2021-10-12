9be21fdc1f7160b03485873bd1502676798ae1a7
LanceStasinski <lance.stasinski@gmail.com>
2021-10-12 15:01:03 -0400

    feat: Add Button component

--- client/src/places/components/PlaceItem.tsx
+++ client/src/places/components/PlaceItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import classes from "./PlaceItem.module.css";
+import Button from "../../shared/components/FormElements/Button";
 
 const PlaceItem: React.FC<{
   id: string;
@@ -23,9 +24,9 @@ const PlaceItem: React.FC<{
           <p>{props.description}</p>
         </div>
         <div className={classes["place-item__actions"]}>
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${props.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
