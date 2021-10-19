b022777adc700e755c54f8570d66f5885f80a237
LanceStasinski <lance.stasinski@gmail.com>
2021-10-19 14:07:53 -0400

    feat: Add delete modal for deleting a place

--- client/src/places/components/PlaceItem.tsx
+++ client/src/places/components/PlaceItem.tsx
@@ -15,11 +15,24 @@ const PlaceItem: React.FC<{
   coordinates: { lat: number; lng: number };
 }> = (props) => {
   const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
 
   const closeMapHandler = () => setShowMap(false);
 
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true)
+  }
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false)
+  }
+
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false)
+    console.log('Deleting...')
+  }
   return (
     <React.Fragment>
       <Modal
@@ -27,12 +40,29 @@ const PlaceItem: React.FC<{
         onCancel={closeMapHandler}
         header={props.address}
         contentClass={classes["place-item__modal-content"]}
-        footerClass={classes['place-item__modal-actions']}
+        footerClass={classes["place-item__modal-actions"]}
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className={classes['map-container']}>
-          <Map center={props.coordinates} zoom={16}/>
+        <div className={classes["map-container"]}>
+          <Map center={props.coordinates} zoom={16} />
         </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass={classes["place-item__modal-actions"]}
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+            <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that this
+          cannot be undone.
+        </p>
       </Modal>
       <li className="place-item">
         <Card className={classes["place-item__content"]}>
@@ -45,9 +75,11 @@ const PlaceItem: React.FC<{
             <p>{props.description}</p>
           </div>
           <div className={classes["place-item__actions"]}>
-            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
           </div>
         </Card>
       </li>
