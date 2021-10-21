d46f61ac3418b91f887e877e4973300976c40cd5
LanceStasinski <lance.stasinski@gmail.com>
2021-10-21 09:41:41 -0400

    refactor: Use body parse to parse incoming requests

--- node_refresher/src/index.ts
+++ node_refresher/src/index.ts
@@ -1,28 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next) => {
-  let body = "";
-  req.on("end", () => {
-    const username = body.split("=")[1];
-    if (username) {
-      req.body = { name: username };
-    }
+app.use(bodyParser.urlencoded({extended: false}));
 
-    next();
-  });
-  req.on("data", (chunk) => {
-    body += chunk;
-  });
-});
+app.post('/user', (req, res, next) => {
+  res.send(`<h1>${req.body.username}</h1>`)
+})
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send(`<h1>${req.body.name}</h1>`);
-  }
+app.get('/', (req, res, next) => {
   res.send(
-    '<form method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>'
+    '<form method="POST" action="/user"><input type="text" name="username" /><button type="submit">Create User</button></form>'
   );
 });
 
