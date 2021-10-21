c859a5dac8c6ff51de4433dc6abb863109089345
LanceStasinski <lance.stasinski@gmail.com>
2021-10-21 09:17:49 -0400

    refactor: Refactor POST request using Express

--- node_refresher/src/index.ts
+++ node_refresher/src/index.ts
@@ -1,26 +1,29 @@
-import { NextFunction } from "express";
+import express from "express";
 
-const http = require("http");
+const app = express();
 
-const server = http.createServer((req: any, res: any, next: any) => {
-  console.log("Incoming request");
-  console.log(req.method, req.url);
+app.use((req, res, next) => {
+  let body = "";
+  req.on("end", () => {
+    const username = body.split("=")[1];
+    if (username) {
+      req.body = { name: username };
+    }
 
-  if (req.method === "POST") {
-    let body: string = "";
-    req.on("end", () => {
-      const userName = body.split("=")[1];
-      res.end(`<h1>${userName}</h1>`);
-    });
-    req.on("data", (chunk: any) => {
-      body += chunk;
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      "<form method='POST'><input type='text' name='username'><button type='submit'>Create User</button></form>"
-    );
-  }
+    next();
+  });
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
 });
 
-server.listen(5000);
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send(`<h1>${req.body.name}</h1>`);
+  }
+  res.send(
+    '<form method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form>'
+  );
+});
+
+app.listen(5000);
