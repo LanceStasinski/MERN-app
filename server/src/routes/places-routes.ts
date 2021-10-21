39c61e75c3b8521f12c00d7f3fa956e646d3f3ca
LanceStasinski <lance.stasinski@gmail.com>
2021-10-21 10:35:48 -0400

    feat: Get place information using request params

--- server/src/routes/places-routes.ts
+++ server/src/routes/places-routes.ts
@@ -2,9 +2,24 @@ import express from 'express'
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET requests in Places');
-  res.json({message: 'It works'})
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'Tall building',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => p.id === placeId)
+  res.json({place})
 })
 
 export default router;
