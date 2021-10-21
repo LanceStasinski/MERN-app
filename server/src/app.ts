75c4c5312fe85879c365a85b0d4275f58315dba8
LanceStasinski <lance.stasinski@gmail.com>
2021-10-21 19:05:41 -0400

    feat: Add error handling for places routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,19 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 
 import placesRoutes from "./routes/places-routes";
+import ServerError from "./util/error";
 //import usersRoutes from './routes/users-routes'
 
 const app = express();
 
 app.use('/api/places', placesRoutes);
 
-
+app.use((err: ServerError, req:Request, res:Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.code || 500).json({message: err.message || 'An unknown error occurred.'})
+})
 
 app.listen(5000);
--- server/src/routes/places-routes.ts
+++ server/src/routes/places-routes.ts
@@ -1,31 +1,43 @@
-import express from 'express'
+import express from "express";
+
+import ServerError from "../util/error";
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'Tall building',
+    id: "p1",
+    title: "Empire State Building",
+    description: "Tall building",
     location: {
       lat: 40.7484405,
-      lng: -73.9878531
+      lng: -73.9878531,
     },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
 ];
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find(p => p.id === placeId)
-  res.json({place})
-})
 
-router.get('/user/:uid', (req, res, next) => {
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  if (!place) {
+    const error  = new ServerError('Could not find place for provided place ID', 404);
+    return next(error) // will work with async code that will be implemented later
+  }
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.find(p => p.creator === userId)
-  res.json({places})
-})
+  const places = DUMMY_PLACES.find((p) => p.creator === userId);
+  if (!places) {
+    const error  = new ServerError('Could not find place for provided user ID', 404);
+    return next(error) // will work with async code that will be implemented later
+  }
+  res.json({ places });
+});
 
 export default router;
