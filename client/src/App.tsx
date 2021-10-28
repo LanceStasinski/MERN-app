c6eee6cc13ea8f27f8ef59faafc5e0a27b7d4b72
LanceStasinski <lance.stasinski@gmail.com>
2021-10-28 10:13:36 -0400

    feat: Add auto-logout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,31 +14,59 @@ import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
 
+let logoutTimer: any;
+
 function App() {
   const [token, setToken] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] =  useState<Date>()
   const [userId, setUserId] = useState(null);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
+    setUserId(uid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       "userData",
-      JSON.stringify({ userId: uid, token: token })
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
     );
-    setUserId(uid);
   }, []);
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData") as string);
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
     }
   }, [login]);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(undefined);
     setUserId(null);
-    localStorage.removeItem('userData');
+    localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime()
+      logoutTimer = setTimeout(logout, remainingTime)
+    } else {
+      clearTimeout(logoutTimer)
+    }
+  }, [token, logout, tokenExpirationDate])
 
   let routes;
 
